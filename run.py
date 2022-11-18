f5d2e496e5de958fda680a8803b863794ec50610
WSMorrison <willimo@gmail.com>
2022-11-18 17:36:48 +0000

    Add a function to clear the terminal between text renders, including importing operating system information and time.

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from os import system, name
+from time import sleep
+# Sleep(x) x is number of seconds.
 
-def game_rules():
-    """
-    Explains game rules, gets user name, and begins gameplay.
-    """
-    print('Game rules')
-
-
+# Basic functions
 def game_quit():
     """
     Ends game when user selects to quit.
@@ -26,6 +23,32 @@ def short_string(string):
     return good_string
 
 
+def clear_screen():
+    """
+    Clears the screen between rending text.
+    """
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+def game_rules():
+    """
+    Explains game rules, gets user name, and begins gameplay.
+    """
+    print('You are in your house.')
+    print('There are Christmas decorations everywhere,')
+    print('and you are disoriented by the blinking, colored lights')
+    print('and the strong smells from the scented candles.\n')
+    print('All the doors in the house are closed,')
+    print('and honestly all look the same.')
+    print('Try a door, and see if it leads to the garage.')
+    sleep(10)
+    clear_screen()
+    print('More')
+
+
 def begin_game():
     """
     Ask user if they want to begin, looped in defense.
@@ -33,8 +56,10 @@ def begin_game():
     user_input = input('\nBegin game? Y/N\n')
     begin = short_string(user_input)
     if begin == 'y':
+        clear_screen()
         game_rules()
     elif begin == 'n':
+        clear_screen()
         game_quit()
     else:
         print('Input must be Y or N')
@@ -45,11 +70,11 @@ def start_game():
     """
     Introduces game and passes user to begin_game function.
     """
-    print('        Welcome to Christmas Vacation.\n')
+    print('          Welcome to Christmas Vacation.\n')
     print('Your entire family has been in your house for a month,')
     print(' and you just want to get out to the garage to relax.')
-    print('     Can you get some valuable alone time')
-    print('          before having a meltdown?')
+    print('       Can you get some valuable alone time')
+    print('            before having a meltdown?')
 
     begin_game()
 
