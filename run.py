1b3b916f875b491fdf63cbb7ceae2114b0bd0a3d
WSMorrison <willimo@gmail.com>
2022-11-18 15:24:37 +0000

    Remove ugly function skeleton per mentor advice.

--- run.py
+++ run.py
@@ -30,74 +30,10 @@ def start_game():
 
     if begin.lower() == 'y':
         game_rules()
-    elif begin == 'n':
+    elif begin.lower == 'n':
         game_quit()
     else:
         print('Input must be Y or N\n')
 
 
 start_game()
-
-
-"""
-GET NAME
-    validate
-        length
-        not blank
-
-EXPLAIN RULES
-    start with 4hp
-    25 positions, one has exit door
-    opponents randomly throughout
-    ready (y/n)
-        validate
-            y or n only
-
-FIRST RENDER
-    decide what position exit door is
-    DISPLAY BOARD
-
-GAMEPLAY
-    user input
-        validate
-            format
-            on board
-            not chosen
-    change board dictionary
-
-    check if exit door
-        WIN
-            access spreadsheet for <win copy>
-
-    check if door is ALLY, CLEAR or OPPONENT, ROLL D6
-        6 ALLY
-            unless two allies chosen, then CLEAR
-            access spreadsheet for <ally copy>
-        4-5 CLEAR
-            access spreadsheet for <clear copy>
-            NEXT
-        1-3 OPPONENT
-            access spreadsheet for <opponent info>
-            user instigate ROLL D6
-            6 - success
-                +1 hp
-                access spreasheet for <opponent success>
-            4-5 - deflect
-                access spreadsheet for <opponent deflect>
-            1-3 - fail, -1 hp
-                -1 hp
-                access spreadsheet for <opponent fail>
-            NEXT
-
-NEXT
-    check player hp
-        hp = 0
-            LOSE
-        hp >= 0
-        increment turn total +1
-        DISPLAY BOARD
-            board a dictionary
-            row is key, value is a list,
-                so it can be changed by targeting a position in the list
-                after a specific key.
-"""
