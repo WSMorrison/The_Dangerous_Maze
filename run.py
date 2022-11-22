1290a2b76ab39de666eb22dee1539bf07e200f70
WSMorrison <willimo@gmail.com>
2022-11-22 09:50:10 +0000

    Reorganized funtions in code so layout is easier to navigate for dev.

--- run.py
+++ run.py
@@ -12,6 +12,58 @@ health_points = 0
 
 
 # Basic functions ------------------------------------------------------
+def short_string(string):
+    """
+    Makes sure input string isn't blank,
+    then shortens user input to one character,
+    and makes sure the string is lowercase
+    as part of defensive design.
+    """
+    nonblank_string = string + 'x'
+    shortened_string = nonblank_string[0]
+    good_string = shortened_string.lower()
+    return good_string
+
+
+def clear_screen():
+    """
+    Clears the screen, often used between rendering text.
+    """
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+def continue_game(next_function):
+    """
+    Y/N/E input asking user if user wants
+    to advance to the next activity.
+    """
+    user_input = input('Y/N/E E for exit.\n')
+    play = short_string(user_input)
+    if play == 'y':
+        clear_screen()
+        if next_function == 'rules':
+            game_rules()
+        elif next_function == 'first':
+            first_render()
+        elif next_function == 'dice':
+            dice_roll()
+        else:
+            print('Oops, there was an error.')
+            game_quit()
+    elif play == 'n':
+        clear_screen()
+        game_quit()
+    elif play == 'e':
+        clear_screen()
+        game_quit()
+    else:
+        print('Input must be Y or N or E.')
+        continue_game(next_function)
+
+
 def game_quit():
     """
     Ends game when user selects to quit.
@@ -26,38 +78,60 @@ def game_over_lose():
     print("You didn't make it, better luck next time.")
 
 
-def short_string(string):
-    """
-    Shortens user input to one lowercase character
-    as first half of defensive design.
-    """
-    nonblank_string = string + 'x'
-    shortened_string = nonblank_string[0]
-    good_string = shortened_string.lower()
-    return good_string
-
-
-def clear_screen():
-    """
-    Clears the screen between rending text.
-    """
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
-
-
 # Game play functions ---------------------------------------------------
+def start_game():
+    """
+    Introduces game and passes user to begin_game function.
+    """
+    clear_screen()
+    print('Welcome to Christmas Vacation.\n')
+    print('Your entire family has been in your house for a month,')
+    print('and you just want to get out to the garage to relax.')
+    print('Can you get some valuable alone time')
+    print('before having a meltdown?')
+    print('\nPlay the game?')
+
+    continue_game('rules')
+
+
+def game_rules():
+    """
+    Explains game rules, gets user name, and begins gameplay.
+    """
+    print('You are in your house.')
+    print('There are Christmas decorations everywhere,')
+    print('and you are disoriented by the blinking, colored lights')
+    print('and the strong smells from the scented candles.\n')
+    print('All the doors in the house are closed,')
+    print('and like this is a bad dream,')
+    print('they all look the same.')
+    print('Try a door, and see if it leads to the garage.\n')
+    print('Behind the door, you may find freedom...')
+    print('...or you may find a needy family member.\n')
+    print('...loading...')
+# DONT FORGET TO CHANGE THE SLEEP TO 10 FOR 10 SECONDS!!!!!!!!
+    sleep(1)
+    clear_screen()
+    print('You will get to roll a D6\n')
+    print('1-4, you lose a health point!')
+    print('5, you escape unscathed,')
+    print('and if you roll a 6, family love and the holiday spirit')
+    print('will restore you one health point.\n')
+    print('Are you ready to go out into the house?')
+
+    continue_game('first')
+
+
 def first_render():
     """
     Does the initial gameboard render
     """
     global health_points
     health_points = 5
-    print('First render happens HERE')
+    print('SYSTEM: First render happens here.')
 # Skipping straight to dice roll for funtion testing.
 # Will need to advance to selecting a door from here.
-    print('Skipping to dice roll\n')
+    print('SYSTEM: Skipping to dice roll.\n')
 
     dice_roll()
 
@@ -68,15 +142,18 @@ def dice_roll():
     """
     global health_points
     roll = random.randint(1, 6)
-    print(f'You rolled a {roll}')
+    print(f'You rolled a {roll}.')
     if roll < 5:
         health_points = health_points - 1
-        print(f'HP -1. Your HP are now {health_points}')
+        print(f'HP -1. Your HP are now {health_points}.')
+        print('\nChoose another door?')
     elif roll == 5:
-        print(f'HP remain the same. Your HP remain {health_points}')
+        print(f'HP remain the same. Your HP remain {health_points}.')
+        print('\nChoose another door?')
     elif roll == 6:
         health_points = health_points + 1
-        print(f'HP +1. Your HP is now {health_points}')
+        print(f'HP +1. Your HP is now {health_points}.')
+        print('\nChoose another door?')
     else:
         print('Error rolling.')
         dice_roll()
@@ -96,80 +173,6 @@ def did_you_die_though():
     else:
         print('Fatal error')
         game_quit()
-
-
-def continue_game(next_function):
-    """
-    Y/N/E input asking user if they want
-    to advance to the next activity.
-    """
-    print('\nContinue?')
-    user_input = input('Y/N/E E for exit.\n')
-    play = short_string(user_input)
-    if play == 'y':
-        clear_screen()
-        if next_function == 'dice':
-            dice_roll()
-        elif next_function == 'first':
-            first_render()
-        elif next_function == 'rules':
-            game_rules()
-        else:
-            print('Oops, there was an error')
-            game_quit()
-    elif play == 'n':
-        clear_screen()
-        game_quit()
-    elif play == 'e':
-        clear_screen()
-        game_quit()
-    else:
-        print('Input must be Y or N or E')
-        continue_game(next_function)
-
-
-def game_rules():
-    """
-    Explains game rules, gets user name, and begins gameplay.
-    """
-    print('You are in your house.')
-    print('There are Christmas decorations everywhere,')
-    print('and you are disoriented by the blinking, colored lights')
-    print('and the strong smells from the scented candles.\n')
-    print('All the doors in the house are closed,')
-    print('and like this is a bad dream,')
-    print('they all look the same.')
-    print('Try a door, and see if it leads to the garage.\n')
-    print('...loading...')
-# DONT FORGET TO CHANGE THE SLEEP TO 10 FOR 10 SECONDS!!!!!!!!
-    sleep(1)
-    clear_screen()
-    print('Try a door, and see if it leads to the garage.')
-    print('Behind the door, you may find freedom....\n')
-    print('...or you may find a needy family member.\n')
-    print('You will get to roll a D6')
-    print('1-4, you lose a health point!')
-    print('5, you escape unscathed,')
-    print('and if you roll a 6, family love and the holiday spirit')
-    print('will restore you one health point.\n')
-    print('Are you ready to go out into the house?')
-
-    continue_game('first')
-
-
-def start_game():
-    """
-    Introduces game and passes user to begin_game function.
-    """
-    clear_screen()
-    print('Welcome to Christmas Vacation.\n')
-    print('Your entire family has been in your house for a month,')
-    print('and you just want to get out to the garage to relax.')
-    print('Can you get some valuable alone time')
-    print('before having a meltdown?')
-    print('\nPlay the game?')
-
-    continue_game('rules')
 
 
 # First call ------------------------------------------------
